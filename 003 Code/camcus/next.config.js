3a443d583d49cf4a530b164e0024883f982181d6
김우곤 <kwgon0102@gmail.com>
2024-04-06 17:04:12 +0900

    PWA 셋팅

---
+++ 003 Code/camcus/next.config.js
@@ -0,0 +1,9 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withPWA(nextConfig);
+// export default withPWA(nextConfig);
