ad7f862bbe0e1ef65d644ef59fa0a0577ed104b8
jjamm00 <112775576+jjamm00@users.noreply.github.com>
2024-05-18 23:48:00 +0900

    Create app.js

---
+++ 003 Code/fling/app/app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import NavigationBar from './NavigationBar';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/messages">
+            <Messages />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Switch>
+        <NavigationBar />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
