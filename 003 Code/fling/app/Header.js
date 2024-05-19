a5015a90f60d592ca781b93dd677bba3e51a8c45
김우곤 <kwgon0102@gmail.com>
2024-05-19 17:49:33 +0900

    mypage 추가

--- 003 Code/fling/app/Header.js
+++ 003 Code/fling/app/Header.js
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="fixed top-0 w-full flex justify-between items-center p-4 bg-white shadow-md">
-      <Link to="/" className="text-xl font-bold">AppName</Link>
+    <header className='fixed top-0 w-full flex justify-between items-center p-4 bg-white shadow-md'>
+      <Link to='/' className='text-xl font-bold'>
+        AppName
+      </Link>
       <nav>
-        <Link to="/settings" className="p-2">설정</Link>
+        <Link to='/settings' className='p-2'>
+          설정
+        </Link>
       </nav>
     </header>
   );
--- 003 Code/fling/app/Nav.js
+++ 003 Code/fling/app/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavigationBar = () => {
+const Nav = () => {
   return (
     <nav className='fixed bottom-0 left-0 right-0 flex justify-around items-center p-4 bg-white shadow-lg'>
       <NavLink to='/' exact className='nav-link' activeClassName='active'>
@@ -20,4 +20,4 @@ const NavigationBar = () => {
   );
 };
 
-export default NavigationBar;
+export default Nav;
