7baa84b1365b2cc4abfb4c1f404053050e633c05
김우곤 <kwgon0102@gmail.com>
2024-09-17 22:03:03 +0900

    Bottom nav 브라우저 히스토리에 남지 않게 replace로 대체 및 간격 변경

--- 003 Code/fling/app/main/BottomNav.js
+++ 003 Code/fling/app/main/BottomNav.js
@@ -1,23 +1,23 @@
 'use client';
 
 import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 
 const BottomNav = () => {
   const [path, setPath] = useState('');
   const currPath = usePathname();
+  const router = useRouter();
   useEffect(() => {
     let pathArr = currPath.split('/main/');
     setPath(pathArr[1]);
   }, [currPath]);
   return (
-    <nav className='w-full h-[80px] max-w-[440px] min-w-[330px] fixed bottom-0 left-1/2 transform -translate-x-1/2 rounded-t-[15px] flex px-[10px] bg-white shadow-inner z-[999999]'>
-      <Link
-        href={'/main'}
-        className='w-1/5 flex flex-col justify-center items-center'
+    <nav className='w-full h-[80px] max-w-[440px] min-w-[330px] fixed bottom-0 left-1/2 transform -translate-x-1/2 rounded-t-[15px] flex px-[10px] pb-[10px] bg-white shadow-inner z-[999999]'>
+      <a
+        onClick={() => router.replace('/main')}
+        className='w-1/5 flex flex-col justify-center items-center cursor-pointer'
       >
         <Image
           src={`/bottomNav/${path === 'home' ? '' : 'un'}checked/home.svg`}
@@ -25,15 +25,10 @@ const BottomNav = () => {
           height={30}
           alt='home'
         />
-        {/* <span
-          className={`text-info text-main-red/50 ${path === 'home' ? null : 'hidden'}`}
-        >
-          홈
-        </span> */}
-      </Link>
-      <Link
-        href={'/main/foodie'}
-        className='w-1/5 flex flex-col justify-center items-center'
+      </a>
+      <a
+        onClick={() => router.replace('/main/foodie')}
+        className='w-1/5 flex flex-col justify-center items-center cursor-pointer'
       >
         <Image
           src={`/bottomNav/${path === 'foodie' ? '' : 'un'}checked/foodie.svg`}
@@ -41,15 +36,10 @@ const BottomNav = () => {
           height={30}
           alt='foodie'
         />
-        {/* <span
-          className={`text-info text-main-red/50 ${path === 'foodie' ? null : 'hidden'}`}
-        >
-          장소추천
-        </span> */}
-      </Link>
-      <Link
-        href={'/main/chat'}
-        className='w-1/5 flex flex-col justify-center items-center'
+      </a>
+      <a
+        onClick={() => router.replace('/main/chat')}
+        className='w-1/5 flex flex-col justify-center items-center cursor-pointer'
       >
         <Image
           src={`/bottomNav/${path === 'chat' ? '' : 'un'}checked/chat.svg`}
@@ -57,16 +47,11 @@ const BottomNav = () => {
           height={30}
           alt='chat'
         />
-        {/* <span
-          className={`text-info text-main-red/50 ${path === 'chat' ? null : 'hidden'}`}
-        >
-          채팅
-        </span> */}
         {/* <div className='size-[5px] bg-main-red rounded-full'></div> */}
-      </Link>
-      <Link
-        href={'/main/mypage'}
-        className='w-1/5 flex flex-col justify-center items-center'
+      </a>
+      <a
+        onClick={() => router.replace('/main/mypage')}
+        className='w-1/5 flex flex-col justify-center items-center cursor-pointer'
       >
         <Image
           src={`/bottomNav/${path === 'mypage' ? '' : 'un'}checked/mypage.svg`}
@@ -74,15 +59,10 @@ const BottomNav = () => {
           height={30}
           alt='mypage'
         />
-        {/* <span
-          className={`text-info text-main-red/50 ${path === 'mypage' ? null : 'hidden'}`}
-        >
-          마이페이지
-        </span> */}
-      </Link>
-      <Link
-        href={'/main/setting'}
-        className='w-1/5 flex flex-col justify-center items-center'
+      </a>
+      <a
+        onClick={() => router.replace('/main/setting')}
+        className='w-1/5 flex flex-col justify-center items-center cursor-pointer'
       >
         <Image
           src={`/bottomNav/${path === 'setting' ? '' : 'un'}checked/setting.svg`}
@@ -90,12 +70,7 @@ const BottomNav = () => {
           height={30}
           alt='setting'
         />
-        {/* <span
-          className={`text-info text-main-red/50 ${path === 'setting' ? null : 'hidden'}`}
-        >
-          설정
-        </span> */}
-      </Link>
+      </a>
     </nav>
   );
 };
