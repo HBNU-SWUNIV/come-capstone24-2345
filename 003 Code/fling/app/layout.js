0569005f502a1c6b07e0e8a15d16db96b50b11b2
김우곤 <kwgon0102@gmail.com>
2024-10-16 21:44:37 +0900

    pwa icon변경

--- 003 Code/fling/app/layout.js
+++ 003 Code/fling/app/layout.js
@@ -25,20 +25,68 @@ export const metadata = {
   title: '플링',
   description: '터치 단 한 번으로 랜덤 소개팅',
   icons: {
-    icon: 'icons/pwa-icons/icon-512x512.png',
+    icon: '/icons/icon.png',
   },
+  manifest: '/manifest.json',
   keywords: ['대학', '소개팅', '랜덤 소개팅', '랜덤'],
-  appleWebApp: {
-    title: '플링',
-    statusBarStyle: 'white',
-    startupImage: [
-      '/assets/startup/apple-touch-startup-image-768x1004.png',
-      {
-        url: '/assets/startup/apple-touch-startup-image-1536x2008.png',
-        media: '(device-width: 768px) and (device-height: 1024px)',
-      },
-    ],
-  },
+  appleTouchStartupImage: [
+    {
+      src: '/icons/splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_portrait.png',
+      media:
+        '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+    },
+    {
+      src: '/icons/splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_portrait.png',
+      media:
+        '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+    },
+    {
+      src: '/icons/splash_screens/Phone_11__iPhone_XR_portrait.png',
+      media:
+        '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+    },
+    {
+      src: '/icons/splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_portrait.png',
+      media:
+        '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+    },
+    {
+      src: '/icons/splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_portrait.png',
+      media:
+        '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+    },
+    {
+      src: '/icons/splash_screens/iPhone_14__iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_portrait.png',
+      media:
+        '(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+    },
+    {
+      src: '/icons/splash_screens/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_portrait.png',
+      media:
+        '(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+    },
+    {
+      src: '/icons/splash_screens/iPhone_16__iPhone_15_Pro__iPhone_15__iPhone_14_Pro_portrait.png',
+      media:
+        '(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+    },
+    {
+      src: '/icons/splash_screens/iPhone_16_Plus__iPhone_15_Pro_Max__iPhone_15_Plus__iPhone_14_Pro_Max_portrait.png',
+      media:
+        '(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+    },
+    {
+      src: '/icons/splash_screens/iPhone_16_Pro_Max_portrait.png',
+      media:
+        '(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+    },
+    {
+      src: '/icons/splash_screens/iPhone_16_Pro_portrait.png',
+      media:
+        '(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+    },
+  ],
+
   other: {
     'mobile-web-app-capable': 'yes',
   },
