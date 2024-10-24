aa01f63cb59c8c47c901c5922978c5144a64d90f
김우곤 <kwgon0102@gmail.com>
2024-10-24 14:11:19 +0900

    favicon추가 및 회원가입 취미선택란 UI 변경

--- 003 Code/fling/app/register/hobby/page.js
+++ 003 Code/fling/app/register/hobby/page.js
@@ -1,43 +1,43 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setStoreHobby } from '../../../library/store';
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setStoreHobby } from "../../../library/store";
 
 const RegisterHobby = () => {
   const [hobby, setHobby] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const hobbyList = [
-    ['영화', 'movie'],
-    ['독서', 'book'],
-    ['여행', 'travel'],
-    ['등산', 'mountain'],
-    ['전시회/박물관', 'museum'],
-    ['헬스/필라테스', 'muscle'],
-    ['수영', 'swim'],
-    ['봉사활동', 'volunteer'],
-    ['요리', 'cooking'],
-    ['원예', 'flower'],
-    ['러닝', 'running'],
-    ['노래', 'microphone'],
-    ['사이클', 'cycle'],
-    ['미술', 'pallet'],
-    ['사진촬영', 'camera'],
-    ['캠핑', 'camping'],
-    ['테니스', 'tennis'],
-    ['스키/보드', 'ski'],
-    ['낚시', 'fishing'],
-    ['게임', 'game'],
-    ['볼링', 'bowling'],
-    ['음주', 'wine'],
-    ['당구', 'billiards'],
-    ['탁구', 'table-tennis'],
-    ['농구', 'basketball'],
-    ['야구', 'baseball'],
-    ['축구', 'soccer'],
+    ["영화", "movie"],
+    ["독서", "book"],
+    ["여행", "travel"],
+    ["등산", "mountain"],
+    ["전시회/박물관", "museum"],
+    ["헬스/필라테스", "muscle"],
+    ["수영", "swim"],
+    ["봉사활동", "volunteer"],
+    ["요리", "cooking"],
+    ["원예", "flower"],
+    ["러닝", "running"],
+    ["노래", "microphone"],
+    ["사이클", "cycle"],
+    ["미술", "pallet"],
+    ["사진촬영", "camera"],
+    ["캠핑", "camping"],
+    ["테니스", "tennis"],
+    ["스키/보드", "ski"],
+    ["낚시", "fishing"],
+    ["게임", "game"],
+    ["볼링", "bowling"],
+    ["음주", "wine"],
+    ["당구", "billiards"],
+    ["탁구", "table-tennis"],
+    ["농구", "basketball"],
+    ["야구", "baseball"],
+    ["축구", "soccer"],
   ];
 
   const router = useRouter();
@@ -59,7 +59,7 @@ const RegisterHobby = () => {
 
   useEffect(() => {
     if (userHobby) {
-      router.replace('/register/etc');
+      router.replace("/register/etc");
     }
   }, [userHobby]);
 
@@ -68,27 +68,27 @@ const RegisterHobby = () => {
       setIsLoading(true);
       dispatch(setStoreHobby(hobby));
     } else {
-      alert('취미를 한 가지 이상 선택해주세요');
+      alert("취미를 한 가지 이상 선택해주세요");
     }
   };
 
   return (
-    <div className='w-full h-dvh px-[40px] pt-[80px] pb-[120px]'>
-      <div className='size-full flex flex-col gap-[20px] relative'>
-        <div className='text-start w-3/5 flex flex-col gap-[10px]'>
-          <p className='text-title text-main-red'>취미</p>
-          <p className='text-subtitle break-keep text-gray-500'>
+    <div className="w-full h-dvh px-[40px] pt-[80px] pb-[120px]">
+      <div className="size-full flex flex-col gap-[20px] relative">
+        <div className="text-start w-3/5 flex flex-col gap-[10px]">
+          <p className="text-title text-main-red">취미</p>
+          <p className="text-subtitle break-keep text-gray-500">
             회원님의 취미를 한 가지 이상 선택해주세요
           </p>
         </div>
 
-        <div className='w-full flex-1 flex justify-center flex-wrap gap-[5px] card-border rounded-medium p-[10px] overflow-y-scroll'>
+        <div className="w-full flex justify-center flex-wrap gap-[5px] p-[10px] overflow-y-scroll">
           {hobbyList.map((item) => {
             return (
               <button
                 key={item[1]}
                 onClick={() => handleHobby(item)}
-                className={`flex justify-center items-center gap-[5px] px-[10px] h-[40px] ${isExist(item) ? 'focus-btn' : 'btn'}`}
+                className={`flex justify-center items-center gap-[5px] px-[10px] h-[40px] ${isExist(item) ? "focus-btn" : "btn"}`}
               >
                 <Image
                   src={`/register/hobby/unchecked/${item[1]}.svg`}
@@ -105,9 +105,9 @@ const RegisterHobby = () => {
         <button
           onClick={handleSubmit}
           disabled={hobby.length === 0}
-          className={`absolute bottom-[-80px] w-full left-0 ${hobby.length === 0 ? 'btn' : 'full-btn'} h-[50px] content-center cursor-pointer`}
+          className={`absolute bottom-[-80px] w-full left-0 ${hobby.length === 0 ? "btn" : "full-btn"} h-[50px] content-center cursor-pointer`}
         >
-          {isLoading ? '확인중...' : '다음'}
+          {isLoading ? "확인중..." : "다음"}
         </button>
       </div>
     </div>
